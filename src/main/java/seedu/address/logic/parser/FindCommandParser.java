29506ad7eed1e8227bfbf17c81a44abf4c8f3294
Victor <victorauyeungxf@gmail.com>
2026-03-17 13:36:53 +0800

    update find command parser

--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,12 +1,23 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_DATE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.location.AddressContainsKeywordsPredicate;
+import seedu.address.model.location.CombinedLocationPredicate;
+import seedu.address.model.location.Location;
 import seedu.address.model.location.NameContainsKeywordsPredicate;
+import seedu.address.model.location.TagMatchesKeywordsPredicate;
+import seedu.address.model.location.VisitDateMatchesKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -19,15 +30,38 @@ public class FindCommandParser implements Parser<FindCommand> {
      * @throws ParseException if the user input does not conform the expected format
      */
     public FindCommand parse(String args) throws ParseException {
-        String trimmedArgs = args.trim();
-        if (trimmedArgs.isEmpty()) {
-            throw new ParseException(
-                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_ADDRESS, PREFIX_TAG, PREFIX_DATE);
+
+        List<Predicate<Location>> predicates = new ArrayList<>();
+
+        // 1. Parse preamble for names (backward compatibility - OR logic)
+        String preamble = argMultimap.getPreamble().trim();
+        if (!preamble.isEmpty()) {
+            String[] nameKeywords = preamble.split("\\s+");
+            predicates.add(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        // 2. Parse Addresses (AND logic)
+        for (String addressKeyword : argMultimap.getAllValues(PREFIX_ADDRESS)) {
+            predicates.add(new AddressContainsKeywordsPredicate(addressKeyword));
+        }
 
-        return new FindCommand(new NameContainsKeywordsPredicate(Arrays.asList(nameKeywords)));
+        // 3. Parse Tags (AND logic for multiple tags)
+        for (String tagKeyword : argMultimap.getAllValues(PREFIX_TAG)) {
+            predicates.add(new TagMatchesKeywordsPredicate(tagKeyword));
+        }
+
+        // 4. Parse Dates (AND logic)
+        for (String dateKeyword : argMultimap.getAllValues(PREFIX_DATE)) {
+            predicates.add(new VisitDateMatchesKeywordsPredicate(dateKeyword));
+        }
+
+        if (predicates.isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        return new FindCommand(new CombinedLocationPredicate(predicates));
     }
 
 }
